8ad41d828b407d18f3f9f8d061b598bd50a1359a
Om Thakur <live.onkar@gmail.com>
2025-10-08 01:05:19 -0400

    .init-file update set up basic logging

---
+++ src/text-summarization/__init__.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import logging 
+
+log_dir="logs"
+log_filepath=os.path.join(log_dir,"running_logs.log")
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s: %(levelname)s: %(module)s: %(message)s]",
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
