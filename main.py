62ec96d5ffd97202b445ee79cc7fa6c2b068f1b6
Om Thakur <live.onkar@gmail.com>
2025-10-11 04:12:02 -0400

    fully functional ingestion pipeline .py format

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-from src.textsummarization import logger
+from src.textsummarization.logging import logger
+from src.textsummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.textsummarization.components.data_ingestion import DataIngestion
 
-logger.info("Logging is implemented")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/textsummarization/components/data_ingestion.py
@@ -0,0 +1,25 @@
+import os
+import urllib.request as request
+import zipfile as zip
+from src.textsummarization.logging import logger
+from src.textsummarization.entity import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                str(self.config.source_URL),
+                str(self.config.local_data_file)
+            )
+            logger.info(f"Downloaded file {filename} with the following headers: {headers}")
+        else:
+            logger.info(f"File already exists of size")
+    def extract_zip_file(self):
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zip.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
+            
